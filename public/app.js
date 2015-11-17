e099be81b982b1707d344b3b093bfca95f67e850
StefKoenig <stefanie.l.koenig@gmail.com>
2015-11-17 11:17:44 -0800

    added app.js for jQuery of to do list, style.css file, and index.html file all of to do list from week 2 day 3

--- public/app.js
+++ public/app.js
@@ -1,2 +1,24 @@
-/////////
-//app.js
+console.log(jQuery);
+
+$('.glyphicon-star').click(function(){
+    ($(this).toggleClass('starClick'))
+    console.log(this);
+});
+
+
+$('input[type="checkbox"]').click(function(){
+    ($(this).parent().toggleClass('strikethrough'));
+    console.log('check');
+});
+
+$('.glyphicon-remove').click(function(){
+    ($(this).parent().remove());
+});
+
+$('.btn').click(function(){
+    console.log('btn click');
+    console.log($('.form-input').val());
+});
+
+//var $newdiv1 = $("<p>"
+$( "<p/ >" ).append( "<strong>Hello</strong>" );
