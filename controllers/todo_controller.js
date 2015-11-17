fd7ed52f6bb45ebbf685491fcf5aa35a2f3959f1
Dan McQuillan <djmcquillan76@gmail.com>
2015-11-17 15:12:59 -0800

    basic controller functions

--- controllers/todo_controller.js
+++ controllers/todo_controller.js
@@ -1,2 +1,29 @@
-/////////////
-//todo_controller.js
+var Todo = require('../models/todo.js')
+
+function index(req, res){
+  Todo.find({}, function(err, todos){
+    if (err) console.log(err)
+    res.json(todos)
+  })
+}
+
+function create(req, res){
+  var todos = new Todo (req.body.todo)
+
+  todo.save(function(err){
+    if (err) console.log(err)
+    res.json({success: true, message: 'Todo Task created!'})
+  })
+}
+
+
+// function destroy(req,res){
+// 	Todo.remove
+// }
+
+
+module.exports = {
+  index: index
+  ,create: create
+}
+
