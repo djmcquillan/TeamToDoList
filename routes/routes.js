6a59f4a8fcf7dddd41f5a74470d6214757740119
Kristen Gunn <ohkristen@yahoo.com>
2015-11-17 12:26:13 -0800

    added the routes for index, create and destroy

--- routes/routes.js
+++ routes/routes.js
@@ -1,2 +1,12 @@
-////////////
-//routes.js
+var express         = require('express'),
+    todoRoutes      = express.Router(),
+    todoController  = require('../controller/todo_controller.js')
+
+/////////////////////////
+// create routes for /api
+todoRoutes.route('/api')
+  .get(todoController.index)
+  .post(todoController.create)
+  .delete(todoController.destroy)
+
+  module.exports = todoRoutes
