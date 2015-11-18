22771e9f9e46ddc7a8c83d9fc9437bc254090ce0
StefKoenig <stefanie.l.koenig@gmail.com>
2015-11-17 18:24:40 -0800

    added ability to get all to do list items with ajax and show them in the view

--- public/app.js
+++ public/app.js
@@ -15,7 +15,41 @@ star.click(function(){
 
 checkboxes.click(function(){
     $(this).next().next().toggleClass('strikethrough');
-})
+});
+
+$(document).ready(function(evt){
+  $.ajax({
+    url:'/api/todos',
+    method: 'GET',
+    success: function(data){
+      data.forEach(function(todo){
+        var li = '<p><input type="checkbox" class="checkboxes"><i class="glyphicon glyphicon-star"></i><span>' + todo.list_item + '</span><i class="glyphicon glyphicon-remove" id="x1"></i></p>'
+        $('.list').last().append(li);
+        $('#todo').val('');
+
+        var newxBox = $('.glyphicon-remove').last()
+        var newstar = $('.glyphicon-star').last()
+        var newcheckboxes = $('.checkboxes').last()
+
+        newxBox.each(function(){
+          newxBox = $('.glyphicon-remove')
+          newxBox.click(function(){
+            $(this).parent().remove();
+          });
+        });
+
+        newstar.click(function(){
+            $(this).toggleClass('active');
+        });
+
+        newcheckboxes.click(function(){
+            $(this).next().next().toggleClass('strikethrough');
+        });
+        event.preventDefault();
+      });
+    }
+  });
+});
 
 $('.btn').on('click', function(evt){
     var list_item = $('#todo').val()
@@ -29,7 +63,6 @@ $('.btn').on('click', function(evt){
       }
     })
 })
-
 
 $('.btn').click(function(){
   var list_item = $('#todo').val()
